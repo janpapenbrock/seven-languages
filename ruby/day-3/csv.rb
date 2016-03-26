8008a448e85f6a5d4f4f05c0df29f7a24d197f43
janpapenbrock <lunoxx@gmail.com>
2016-03-26 12:46:22 +0100

    Ruby, day 3, exercise solution.

--- ruby/day-3/csv.rb
+++ ruby/day-3/csv.rb
@@ -4,7 +4,6 @@ module ActsAsCsv
     base.extend ClassMethods
   end
 
-
   module ClassMethods
     def acts_as_csv
       include InstanceMethods
@@ -27,6 +26,26 @@ module ActsAsCsv
     def initialize
       read
     end
+
+    def rows
+      @rows = @rows ||@csv_contents.collect do |row|
+        CsvRow.new(row, @headers)
+      end
+    end
+
+    def each &block
+      rows.each &block
+    end
+  end
+end
+
+class CsvRow
+  def initialize values, keys
+    @lookup = Hash[keys.zip(values)]
+  end
+
+  def method_missing name
+    puts @lookup[name.to_s]
   end
 end
 
@@ -38,3 +57,6 @@ end
 m = RubyCsv.new
 puts m.headers.inspect
 puts m.csv_contents.inspect
+
+m.each {|row| puts row.one}
+m.each {|row| puts row.two}
